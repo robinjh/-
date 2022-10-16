68a169053ddaaf355342e4a6948b3e2182a5301d
Jaehwan Kim <59593345+robinjh@users.noreply.github.com>
2022-10-16 19:27:31 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+int add(int a, int b) {
+	int temp;
+	temp = a + b;
+	return temp;
+}
+int main() {
+	int a, b;
+	scanf("%d %d", &a, &b);
+	int result;
+	result = add(a, b);
+
+	printf("%d",result);
+	return 0;
+}
